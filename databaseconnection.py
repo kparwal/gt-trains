6b31a3382fb975ae4825867c55e9f639a49f6da1
rikhilshah <rikhilrshah@gmail.com>
2016-04-19 00:37:47 -0400

    Added files via upload
    To populate databases

---
+++ databaseconnection.py
@@ -0,0 +1,16 @@
+def Connect():
+    try:
+        connection = pymysql.connect(host = 'academic-mysql.cc.gatech.edu', passwd = "k7E1wfyD", user = "cs4400_Team_73", db = "cs4400_Team_73")
+        cursor = connection.cursor()
+        return connection
+    except:
+        print("Cannot connect to database")
+
+data = self.Connect()
+cursor = data.cursor()
+query = 'INSERT INTO Travelers(Username,Password,LastName)VALUES("{0}","{1}","{2}")'.format(self.user.get(),self.pass1.get(),self.name.get())
+cursor.execute(query)
+userpass = cursor.fetchone()
+data.commit()
+cursor.close()
+data.close()
