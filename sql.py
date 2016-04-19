239103df3b6e7e38a45c79be73f9af9be1f0e7af
Hasit Dewan <Hasit@lawn-128-61-118-214.lawn.gatech.edu>
2016-04-19 13:06:48 -0400

    minor edits, and fixed register page

--- sql.py
+++ sql.py
@@ -4,8 +4,8 @@ import pymysql
 from re import findall
 import datetime
 
-class RankTheCities:
-    
+class GTTrains:
+
     def __init__(self,win):
         self.win = win
         self.image = PhotoImage(file="Buzz.gif")
@@ -19,15 +19,24 @@ class RankTheCities:
         #self.username.set("A")
         #self.password.set("A1")
         self.win.title("Login")
-        
-        lb0 = Label(self.frame, text = "Username:").grid(row=1,column=0, sticky=W)
-        e0 = Entry(self.frame, textvariable = self.username).grid(row=1,column=1, columnspan=2)
-        
-        lb1 = Label(self.frame, text = "Password:").grid(row=2,column=0, sticky=W)
-        e1 = Entry(self.frame, textvariable = self.password, show ="*").grid(row=2,column=1, columnspan=2)
-        
-        b1 = Button(self.frame, text = "Register", command = self.register).grid(row=3,column=0, sticky=EW)
-        b2 = Button(self.frame, text = "Login", command = self.login).grid(row=3,column=1, sticky=EW)
+
+        lb0 = Label(self.frame, text = "Username:")
+        lb0.grid(row=1,column=0, sticky=W)
+
+        e0 = Entry(self.frame, textvariable = self.username)
+        e0.grid(row=1,column=1, columnspan=2)
+
+        lb1 = Label(self.frame, text = "Password:")
+        lb1.grid(row=2,column=0, sticky=W)
+
+        e1 = Entry(self.frame, textvariable = self.password, show ="*")
+        e1.grid(row=2,column=1, columnspan=2)
+
+        b1 = Button(self.frame, text = "Register", command = self.register)
+        b1.grid(row=3,column=0, sticky=EW)
+
+        b2 = Button(self.frame, text = "Login", command = self.login)
+        b2.grid(row=3,column=1, sticky=EW)
 
     def register(self):
         self.win.withdraw()
@@ -36,29 +45,52 @@ class RankTheCities:
         canvas.pack()
         canvas.create_image(50,50, anchor=CENTER, image=self.image)
         win2 = Frame(self.winregister)
-        win2.pack()
-        
+
         self.name = StringVar()
         self.user = StringVar()
         self.pass1 = StringVar()
         self.pass2 = StringVar()
         self.entrys = [self.name, self.user, self.pass1, self.pass2]
-        
-        lb0 = Label(win2, text = "Last Name:").grid(row=0,column=0, sticky=W)
-        e0 = Entry(win2, textvariable = self.name).grid(row=0,column=1, columnspan=4)
-        
-        lb1 = Label(win2, text = "Username:").grid(row=1,column=0, sticky=W)
-        e1 = Entry(win2, textvariable = self.user).grid(row=1,column=1, columnspan=4)
-        
-        lb2 = Label(win2, text = "Password:").grid(row=2,column=0, sticky=W)
-        e2 = Entry(win2, textvariable = self.pass1, show ="*").grid(row=2,column=1, columnspan=4)
-        
-        lb2 = Label(win2, text = "Confirm Password:").grid(row=3,column=0, sticky=W)
-        e2 = Entry(win2, textvariable = self.pass2, show ="*").grid(row=3,column=1, columnspan=4)
-        
-        b1 = Button(win2, text = "Cancel", command = self.cancel).grid(row=4,column=2, sticky=EW)
-        b2 = Button(win2, text = "Register", command = self.checkregistration).grid(row=4,column=3,columnspan=2, sticky=EW)
-    
+
+        lb0 = Label(win2, text = "Last Name:")
+        lb0.grid(row=0,column=0, sticky=W)
+
+        e0 = Entry(win2, textvariable = self.name)
+        e0.grid(row=0,column=1, columnspan=4)
+
+        lb1 = Label(win2, text = "Username:")
+        lb1.grid(row=1,column=0, sticky=W)
+
+        e1 = Entry(win2, textvariable = self.user)
+        e1.grid(row=1,column=1, columnspan=4)
+
+        lb2 = Label(win2, text = "Password:")
+        lb2.grid(row=2,column=0, sticky=W)
+
+        e2 = Entry(win2, textvariable = self.pass1, show ="*")
+        e2.grid(row=2,column=1, columnspan=4)
+
+        lb2 = Label(win2, text = "Confirm Password:")
+        lb2.grid(row=3,column=0, sticky=W)
+
+        e2 = Entry(win2, textvariable = self.pass2, show ="*")
+        e2.grid(row=3,column=1, columnspan=4)
+
+        b1 = Button(win2, text = "Cancel", command = self.cancel)
+        b1.grid(row=4,column=2, sticky=EW)
+
+        b2 = Button(win2, text = "Register", command = self.checkRegistration)
+        b2.grid(row=4,column=3,columnspan=2, sticky=EW)
+
+        win2.pack()
+
+    def cancel(self):
+        print("hi")
+
+    def checkRegistration(self):
+        print("hi")
+
+
     def login(self):
         data = self.Connect()
         cursor = data.cursor()
@@ -66,8 +98,8 @@ class RankTheCities:
         userpass = cursor.fetchone()
         cursor.close()
         data.close()
-        if userpass != () and userpass != None:
-            if self.password.get()==userpass[0]:
+        if (userpass != () and userpass != None):
+            if (self.password.get() == userpass[0]):
                 messagebox.showinfo("Login Successful!", "You are now logged in")
                 self.win.withdraw()
                 #calls the big window with the user data
@@ -86,5 +118,5 @@ class RankTheCities:
             print("Cannot connect to database")
 
 window = Tk()
-rankObj = RankTheCities(window)
+rankObj = GTTrains(window)
 window.mainloop()
